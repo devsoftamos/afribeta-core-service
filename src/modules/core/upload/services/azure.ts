77165d756340ec52f15158f1b77f2e67d09fb9c2
Leona-cloud <chisomokafor038@gmail.com>
2023-10-24 00:17:59 +0100

    chore: create function to delete file from azure blob storage

--- src/modules/core/upload/services/azure.ts
+++ src/modules/core/upload/services/azure.ts
@@ -3,6 +3,7 @@ import { BlobServiceClient, BlockBlobClient } from "@azure/storage-blob";
 import { Injectable } from "@nestjs/common";
 import {
     CompressImageOptions,
+    DeleteFileOptions,
     IUploadService,
     ImageFormat,
     UploadFileOptions,
@@ -54,5 +55,27 @@ export class AzureStorageService
         const blockBlobClient = this.getBlockBlobClient(blobName, options.dir);
         await blockBlobClient.uploadData(compressedBuffer);
         return blockBlobClient.url;
+    }
+
+    async deleteFile(options: DeleteFileOptions) {
+        const connectionString = this.buildConnectionString();
+        const blobServiceClient =
+            BlobServiceClient.fromConnectionString(connectionString);
+        const containerClient = blobServiceClient.getContainerClient(
+            options.dir
+        );
+        const deleteOptions = {
+            deleteSnapshots: "include",
+        };
+        const blobUrl = options.blobUrl;
+        const blobName = blobUrl.split("/").slice(-1)[0];
+        try {
+            await containerClient.getBlobClient(blobName).delete(deleteOptions);
+            console.log(`Blob "${blobName}" deleted successfully.`);
+        } catch (error) {
+            console.error(
+                `Error deleting blob "${blobName}": ${error.message}`
+            );
+        }
     }
 }
