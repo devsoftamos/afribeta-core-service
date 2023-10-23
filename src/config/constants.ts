667d6a7c11db50307653c5907086b966ec160041
calculusky <calculusky@gmail.com>
2023-10-23 14:35:38 +0100

    feat: updates on config

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,3 +4,4 @@ export const MAIL_SENDER_EMAIL = " gostack.africa@gmail.com";
 export const DB_TRANSACTION_TIMEOUT = 10000;
 export const AFRIBETA_WALLET_NAME = "AfriB Wallet";
 export const COMPANY_NAME = "AfricaBeta";
+export const KYC_UPLOAD_DIR = "kycInfo";
