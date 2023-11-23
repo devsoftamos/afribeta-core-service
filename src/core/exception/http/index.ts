a1f2a98cf353beddaac2250c097bd12a62bffcd2
calculusky <calculusky@gmail.com>
2023-11-23 10:48:38 +0100

    chore: added log on exception module

--- src/core/exception/http/index.ts
+++ src/core/exception/http/index.ts
@@ -17,6 +17,7 @@ export class AllExceptionsFilter implements ExceptionFilter {
         // In certain situations `httpAdapter` might not be available in the
         // constructor method, thus we should resolve it here.
 
+        console.log(exception);
         const { httpAdapter } = this.httpAdapterHost;
 
         const ctx = host.switchToHttp();
