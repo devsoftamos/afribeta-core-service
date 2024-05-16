df6303bbb4ccb78b7776bbcc44ac98c16fe8fd5b
calculusky <calculusky@gmail.com>
2024-05-16 12:52:25 +0100

    chore: reverted and refactored

--- src/modules/scheduler/index.ts
+++ src/modules/scheduler/index.ts
@@ -1,16 +1,9 @@
 import { Module, forwardRef } from "@nestjs/common";
 import { WalletModule } from "../api/wallet";
 import { WalletSchedulerService } from "./services/wallet";
-import { BullModule } from "@nestjs/bull";
-import { billBoardQueueConfig, scheduleQueueConfig } from "./queues";
-import { BullBoardModule } from "@bull-board/nestjs";
 
 @Module({
-    imports: [
-        forwardRef(() => WalletModule),
-        BullModule.registerQueue(...scheduleQueueConfig),
-        BullBoardModule.forFeature(...billBoardQueueConfig),
-    ],
+    imports: [forwardRef(() => WalletModule)],
     providers: [WalletSchedulerService],
     exports: [WalletSchedulerService],
 })
