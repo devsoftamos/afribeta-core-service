8cea2918555e99d51e0cb3cb4bc4f74dced22988
calculusky <calculusky@gmail.com>
2024-09-11 19:23:09 +0100

    chore: updates on webhook module

--- src/modules/webhook/index.ts
+++ src/modules/webhook/index.ts
@@ -1,34 +1,15 @@
 import { Module } from "@nestjs/common";
 import { RouterModule } from "@nestjs/core";
-// import { FSDH360BankWebhookModule } from "./fsdh360Bank";
 import { PaystackWebhookModule } from "./paystack";
-// import { ProvidusWebhookModule } from "./providus";
-// import { SquadGTBankWebhookModule } from "./squadGTBank";
 
-//TODO: Remove the NUBAN Banks module. Webhook moved to API Service
 @Module({
     imports: [
-        // ProvidusWebhookModule,
         PaystackWebhookModule,
-        // SquadGTBankWebhookModule,
-        // FSDH360BankWebhookModule,
         RouterModule.register([
             {
                 path: "webhook",
                 module: PaystackWebhookModule,
             },
-            // {
-            //     path: "webhook",
-            //     module: ProvidusWebhookModule,
-            // },
-            // {
-            //     path: "webhook",
-            //     module: SquadGTBankWebhookModule,
-            // },
-            // {
-            //     path: "webhook",
-            //     module: FSDH360BankWebhookModule,
-            // },
         ]),
     ],
 })
